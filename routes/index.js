39b8967d466b689e6dbdc00f107c70f2b12e4108
tejuko <tessjuliette02@gmail.com>
2026-03-24 12:56:43 +0100

    password reset werkte ook niet meer, deze heb ik weer toegevoegd

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ const dashboardRouter = require('./dashboard');
 const matchesRouter = require('./matches');
 const usersRouter = require('./users');
 const apiRouter = require('./api');
+const passwordResetRouter = require('./password-reset');
 
 router.use(pagesRouter);
 router.use(authRouter);
@@ -14,5 +15,6 @@ router.use(dashboardRouter);
 router.use(matchesRouter);
 router.use(usersRouter);
 router.use(apiRouter);
+router.use(passwordResetRouter);
 
 module.exports = router;
